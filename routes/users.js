80511a2b914e8146b903ed5e2dc137d1ee040f28
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-03-09 11:49:37 +0100

    Facebook login toegevoegd. Werkt nog niet

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 module.exports = function(passport, mongoose){
 
 	router.post('/login', passport.authenticate('local-login'), function(req, res) {
 		res.send(req.user);
   	});
+
+    app.get('/auth/facebook', passport.authenticate('facebook', {
+        scope : ['public_profile', 'email']
+    }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
 
 	router.post('/signup', passport.authenticate('local-signup'), function(req, res) {
 		res.send(req.user);
