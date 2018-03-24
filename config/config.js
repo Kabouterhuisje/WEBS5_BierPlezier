b1a1c1a59e88ed2106c73b3fefd0a5aef536ebdc
Dennis Tijbosch <Kabouterhuisje@users.noreply.github.com>
2018-03-25 00:55:35 +0100

    Updating connectionString to Heroku / mLab location

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	db: {
-		development: 'mongodb://127.0.0.1:27017/webs5-eindopdracht',
+		development: 'mongodb://kabouterhuisje:P@ssword@ds223009.mlab.com:23009/bier-plezier',
 		test: 'mongodb://127.0.0.1:27017/webs5-test'
 	}
 };
