3fb1586e7fb5b79bba2d5901d63cd41caa093d4b
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-03-25 01:04:32 +0100

    password bug fix

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	db: {
-		development: 'mongodb://127.0.0.1:27017/webs5-eindopdracht',
+		development: 'mongodb://kabouterhuisje:12345@ds223009.mlab.com:23009/bier-plezier',
 		test: 'mongodb://127.0.0.1:27017/webs5-test'
 	}
 };
